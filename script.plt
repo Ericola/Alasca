0098fe87ce1dd83f9efa9f398fd50f1ce5cf0caa
Eric <eric-lim@hotmail.fr>
2015-12-16 01:12:09 +0100

    Modif script.plt

--- script.plt
+++ script.plt
@@ -1,5 +1,6 @@
-set title "Variations du temps moyen d'éxécution d'une requête au cours du temps"
-set xlabel "Numéro des moyennes reçues"
-set ylabel "Temps moyen d'éxécution d'une requête (ms)"
-plot "Courbe.txt" using 1:2 title 'Temps moyen' with linespoints 
+set title "Variations du temps moyen d'execution d'une requete au cours du temps"
+set xlabel "Numero des moyennes recues"
+set ylabel "Temps moyen d'execution d'une requete (ms)"
+plot "Courbe.txt" using 1:2 title 'Temps moyen' with linespoints, \
+"Courbe.txt" using 1:3 title 'Adaptation'
 
--- src/fr/upmc/datacenter/software/controller/Controller.java
+++ src/fr/upmc/datacenter/software/controller/Controller.java
@@ -19,118 +19,123 @@ import fr.upmc.datacenter.software.requestdispatcher.ports.RequestDispatcherDyna
 
 public class Controller extends AbstractComponent implements RequestDispatcherStateDataConsumerI {
 
-    protected static final long THRESHOLD_AVG_ADJUSTMENT_MS = 5000;
-    protected static final long DURATION_BETWEEN_ADJUSTMENT = 1000000000L;
-    public static final boolean TRACE_GRAPH                 = false;
+	protected static final long THRESHOLD_AVG_ADJUSTMENT_MS = 5000;
+	protected static final long DURATION_BETWEEN_ADJUSTMENT = 1000000000L;
+	public static final boolean TRACE_GRAPH                 = true;
 
-    /** the URI of the component. */
-    protected String             cURI;
-    protected ScheduledFuture<?> pullingFuture;
+	/** the URI of the component. */
+	protected String             cURI;
+	protected ScheduledFuture<?> pullingFuture;
 
-    protected RequestDispatcherDynamicStateDataOutboundPort requestDispatcherDynamicStateDataOutboundPort;
+	protected RequestDispatcherDynamicStateDataOutboundPort requestDispatcherDynamicStateDataOutboundPort;
 
-    /** OutboundPort uses to communicate with the AdmissionController */
-    protected AdmissionControllerManagementOutboundPort acmop;
-    protected final static String                       Filename  = "Courbe.txt";
-    public static int                                   nbMoyRecu = 0;
+	/** OutboundPort uses to communicate with the AdmissionController */
+	protected AdmissionControllerManagementOutboundPort acmop;
+	protected final static String                       Filename  = "Courbe.txt";
+	public static int                                   nbMoyRecu = 0;
 
-    protected Long lastAllocatedVM = 0l;
+	protected Long lastAllocatedVM = 0l;
 
-    boolean x = false;
+	boolean x = false;
 
-    public Controller( String cURI , String requestDispatcherURI , String admissionControllerManagementOutboundPortURI ,
-            String rddsdip ) throws Exception {
-        super( true , true );
-        this.cURI = cURI;
-        this.requestDispatcherDynamicStateDataOutboundPort = new RequestDispatcherDynamicStateDataOutboundPort( this ,
-                requestDispatcherURI );
-        this.addRequiredInterface( DataRequiredI.PullI.class );
-        this.addOfferedInterface( DataRequiredI.PushI.class );
-        this.addRequiredInterface( ControlledDataRequiredI.ControlledPullI.class );
-        this.addPort( this.requestDispatcherDynamicStateDataOutboundPort );
-        this.requestDispatcherDynamicStateDataOutboundPort.publishPort();
-        this.requestDispatcherDynamicStateDataOutboundPort.doConnection( rddsdip ,
-                DataConnector.class.getCanonicalName() );
+	public Controller( String cURI , String requestDispatcherURI , String admissionControllerManagementOutboundPortURI ,
+			String rddsdip ) throws Exception {
+		super( true , true );
+		this.cURI = cURI;
+		this.requestDispatcherDynamicStateDataOutboundPort = new RequestDispatcherDynamicStateDataOutboundPort( this ,
+				requestDispatcherURI );
+		this.addRequiredInterface( DataRequiredI.PullI.class );
+		this.addOfferedInterface( DataRequiredI.PushI.class );
+		this.addRequiredInterface( ControlledDataRequiredI.ControlledPullI.class );
+		this.addPort( this.requestDispatcherDynamicStateDataOutboundPort );
+		this.requestDispatcherDynamicStateDataOutboundPort.publishPort();
+		this.requestDispatcherDynamicStateDataOutboundPort.doConnection( rddsdip ,
+				DataConnector.class.getCanonicalName() );
 
-        this.addRequiredInterface( AdmissionControllerManagementI.class );
-        this.acmop = new AdmissionControllerManagementOutboundPort( admissionControllerManagementOutboundPortURI ,
-                this );
-        this.addPort( this.acmop );
-        this.acmop.publishPort();
-        if ( TRACE_GRAPH ) {
-            FileWriter f = new FileWriter( Filename , false );
-            f.close();
-        }
+		this.addRequiredInterface( AdmissionControllerManagementI.class );
+		this.acmop = new AdmissionControllerManagementOutboundPort( admissionControllerManagementOutboundPortURI ,
+				this );
+		this.addPort( this.acmop );
+		this.acmop.publishPort();
+		if ( TRACE_GRAPH ) {
+			FileWriter f = new FileWriter( Filename , false );
+			f.close();
+		}
 
-    }
+	}
 
-    public void startControlling() throws Exception {
+	public void startControlling() throws Exception {
 
-        this.pullingFuture = this.scheduleTaskAtFixedRate( new ComponentI.ComponentTask() {
+		this.pullingFuture = this.scheduleTaskAtFixedRate( new ComponentI.ComponentTask() {
 
-            @Override
-            public void run() {
-                try {
+			@Override
+			public void run() {
+				try {
 
-                    RequestDispatcherDynamicStateI rdds = getDynamicState();
-                    print( "timestamp      : " + rdds.getTimeStamp() );
-                    print( "timestamper id : " + rdds.getTimeStamperId() );
-                    print( "request time average : " + rdds.getRequestProcessingAvg() + " ms" );
-                    if ( rdds.getRequestProcessingAvg() != 0 ) {
-                        if ( TRACE_GRAPH ) {
-                            try {
-                                FileWriter fw = new FileWriter( Filename , true );
-                                fw.write( nbMoyRecu + " " + rdds.getRequestProcessingAvg() + "\n" );
-                                nbMoyRecu++;
-                                fw.close();
-                            }
-                            catch ( IOException exception ) {
-                                System.out.println( "Erreur lors de l'ecriture : " + exception.getMessage() );
-                            }
-                        }
-                    }
+					RequestDispatcherDynamicStateI rdds = getDynamicState();
+					print( "timestamp      : " + rdds.getTimeStamp() );
+					print( "timestamper id : " + rdds.getTimeStamperId() );
+					print( "request time average : " + rdds.getRequestProcessingAvg() + " ms" );
+					boolean adaptation = false;
 
-                    if ( System.nanoTime() - lastAllocatedVM > DURATION_BETWEEN_ADJUSTMENT
-                            && rdds.getRequestProcessingAvg() > THRESHOLD_AVG_ADJUSTMENT_MS ) {
-                        acmop.increaseFrequency();
-                        if ( !acmop.addCores( rdds.getRequestDispatcherURI() , 2 ) )
-                            acmop.allocateVM( rdds.getRequestDispatcherURI() );
-                        lastAllocatedVM = System.nanoTime();
-                        // acmop.removeVM(rdds.getRequestDispatcherURI());
-                        x = true;
-                    }
+					if ( System.nanoTime() - lastAllocatedVM > DURATION_BETWEEN_ADJUSTMENT
+							&& rdds.getRequestProcessingAvg() > THRESHOLD_AVG_ADJUSTMENT_MS ) {
+						acmop.increaseFrequency();
+						if ( !acmop.addCores( rdds.getRequestDispatcherURI() , 2 ) )
+							acmop.allocateVM( rdds.getRequestDispatcherURI() );
+						lastAllocatedVM = System.nanoTime();
+						// acmop.removeVM(rdds.getRequestDispatcherURI());
+						adaptation = true;
+					}
 
-                }
-                catch ( Exception e ) {
-                    e.printStackTrace();
-                }
-            }
-        } , 1l , 1l , TimeUnit.SECONDS );
-    }
+					if ( rdds.getRequestProcessingAvg() != 0 ) {
+						if ( TRACE_GRAPH ) {
+							try {
+								FileWriter fw = new FileWriter( Filename , true );
+								if(!adaptation)
+									fw.write( nbMoyRecu + " " + rdds.getRequestProcessingAvg() + "\n" );
+								else
+									fw.write( nbMoyRecu + " " + rdds.getRequestProcessingAvg() + " " + rdds.getRequestProcessingAvg() + "\n" );
+								nbMoyRecu++;
+								fw.close();
+							}
+							catch ( IOException exception ) {
+								System.out.println( "Erreur lors de l'ecriture : " + exception.getMessage() );
+							}
+						}
+					}
 
-    public RequestDispatcherDynamicStateI getDynamicState() throws Exception {
-        return ( RequestDispatcherDynamicStateI ) requestDispatcherDynamicStateDataOutboundPort.request();
-    }
+				}
+				catch ( Exception e ) {
+					e.printStackTrace();
+				}
+			}
+		} , 1l , 1l , TimeUnit.SECONDS );
+	}
 
-    private void print( String s ) {
-        this.logMessage( "[Controller] " + s );
-    }
+	public RequestDispatcherDynamicStateI getDynamicState() throws Exception {
+		return ( RequestDispatcherDynamicStateI ) requestDispatcherDynamicStateDataOutboundPort.request();
+	}
 
-    @Override
-    public void shutdown() throws ComponentShutdownException {
-        try {
-            if ( this.acmop.connected() )
-                this.acmop.doDisconnection();
-        }
-        catch ( Exception e ) {
-            throw new ComponentShutdownException( e );
-        }
-        super.shutdown();
-    }
+	private void print( String s ) {
+		this.logMessage( "[Controller] " + s );
+	}
 
-    @Override
-    public void acceptRequestDispatcherDynamicData( String requestDispatcherURI ,
-            RequestDispatcherDynamicStateI currentDynamicState ) throws Exception {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void shutdown() throws ComponentShutdownException {
+		try {
+			if ( this.acmop.connected() )
+				this.acmop.doDisconnection();
+		}
+		catch ( Exception e ) {
+			throw new ComponentShutdownException( e );
+		}
+		super.shutdown();
+	}
+
+	@Override
+	public void acceptRequestDispatcherDynamicData( String requestDispatcherURI ,
+			RequestDispatcherDynamicStateI currentDynamicState ) throws Exception {
+		// TODO Auto-generated method stub
+	}
 }
